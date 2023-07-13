e35fe94dd980809ec69960c9455e9661a922fc0d
Kornull <wolod1987kor@gmail.com>
2023-07-13 20:16:31 +0300

    fix: path

--- product-list/src/components/App/App.tsx
+++ product-list/src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router';
 
-import { MainPage } from '../../pages/Main';
+import { MainPage } from 'src/pages/Main';
 import { Layout } from '../Layout';
-import { NotFoundPage } from '../../pages/NotFOund';
-import { ProductPage } from '../../pages/Product/ProductPage';
+import { NotFoundPage } from 'src/pages/NotFOund';
+import { ProductPage } from 'src/pages/Product/ProductPage';
 
-import { useGlobalState } from '../../state';
-import { STATE_DATA_CALLS } from '../../types';
+import { useGlobalState } from 'src/state';
+import { STATE_DATA_CALLS } from 'src/types';
 
 function App() {
   const [product] = useGlobalState(STATE_DATA_CALLS.PRODUCT);
